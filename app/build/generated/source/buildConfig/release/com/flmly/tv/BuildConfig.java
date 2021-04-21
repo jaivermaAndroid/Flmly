1b64264c4656553daf19a3680ebc358e5133eb6c
jaivermaAndroid <jai.developer1@gmail.com>
2021-04-21 22:41:37 +0530

    Search ui update

---
+++ app/build/generated/source/buildConfig/release/com/flmly/tv/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.flmly.tv;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.flmly.tv";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "1.0";
+}
